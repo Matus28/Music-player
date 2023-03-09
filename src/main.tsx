09da971c8f17afd0189ef2f263078db2546eb79e
Matus Tatarko <matus.tatarko28@gmail.com>
2023-03-09 18:46:46 +0100

    feat: adds context for serving information about playlists and to update playlist data

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { PlaylistsProvider } from "./context/PlaylistsContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <PlaylistsProvider>
+      <App />
+    </PlaylistsProvider>
   </React.StrictMode>
 );
