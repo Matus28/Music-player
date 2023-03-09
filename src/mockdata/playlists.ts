061f90ff7090c11c155ea582056461695a956854
Matus Tatarko <matus.tatarko28@gmail.com>
2023-03-09 18:37:34 +0100

    refactor: moves mock data to separate directory

---
+++ src/mockdata/playlists.ts
@@ -0,0 +1,46 @@
+import { Playlist } from "../../utils/types";
+
+export const playlistData: Playlist[] = [
+  {
+    playlistId: 1,
+    playlistTitle: "All tracks",
+    playlistAuthorId: "007",
+    playlistIsDeletable: false,
+  },
+  {
+    playlistId: 2,
+    playlistTitle: "Favorites",
+    playlistAuthorId: "007",
+    playlistIsDeletable: false,
+  },
+  {
+    playlistId: 3,
+    playlistTitle: "My mix tralala",
+    playlistAuthorId: "007",
+    playlistIsDeletable: true,
+  },
+  {
+    playlistId: 4,
+    playlistTitle: "Boom shakalaka",
+    playlistAuthorId: "007",
+    playlistIsDeletable: true,
+  },
+  // {
+  //   playlistId: 5,
+  //   playlistTitle: "OMG",
+  //   playlistAuthorId: "007",
+  //   playlistIsDeletable: true,
+  // },
+  // {
+  //   playlistId: 6,
+  //   playlistTitle: "Random mix",
+  //   playlistAuthorId: "007",
+  //   playlistIsDeletable: true,
+  // },
+  // {
+  //   playlistId: 7,
+  //   playlistTitle: "Random mix",
+  //   playlistAuthorId: "007",
+  //   playlistIsDeletable: true,
+  // },
+];
