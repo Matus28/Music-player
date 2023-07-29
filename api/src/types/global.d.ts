59776097d3001555209526c7f516d51b66ec3434
Matus Tatarko <matus.tatarko28@gmail.com>
2023-07-29 15:46:31 +0200

    feat: creates file for declaration of types used by typescript

---
+++ api/src/types/global.d.ts
@@ -0,0 +1,57 @@
+import { Song } from "../schema/song";
+
+export {};
+
+declare global {
+  interface SongDriveData {
+    id: string;
+    name: string;
+    url: string;
+    download: string;
+    folders: Array<string | null | undefined>;
+    cover: string;
+  }
+
+  interface Metadata {
+    name: string;
+    author: string;
+    duration: string;
+  }
+
+  interface SongData {
+    songDriveId: string;
+    songName: string;
+    songArtist: string;
+    songUrl: string;
+    songCover: string;
+    songLength: string;
+  }
+
+  interface UserId {
+    userId: number;
+  }
+
+  namespace Express {
+    export interface Request {
+      user?: UserId;
+    }
+  }
+
+  namespace Express {
+    export interface Response {
+      message?: string;
+    }
+  }
+
+  export type PlaylistElement = {
+    playlistId: number;
+    playlistName: string;
+    playlistOwner: number;
+    playlistDeletable: boolean;
+    playlistSongs: Song[];
+  };
+
+  export type Library = {
+    playlists: PlaylistElement[];
+  };
+}
