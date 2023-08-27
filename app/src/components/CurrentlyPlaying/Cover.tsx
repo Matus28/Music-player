bb127bf9076cc43a8e9d26f1c8b331776d381257
Matus Tatarko <matus.tatarko28@gmail.com>
2023-08-28 01:37:17 +0200

    refactor: imports Placeholder image

--- app/src/components/CurrentlyPlaying/Cover.tsx
+++ app/src/components/CurrentlyPlaying/Cover.tsx
@@ -1,12 +1,11 @@
 import styles from "./Cover.module.scss";
+import Placeholder from "../../../public/img/music-placeholder.png";
 
 export const Cover = (props: { path?: string }): JSX.Element => {
   return (
     <div className={styles.cover}>
       <img
-        src={
-          props.path ? props.path : "../../../public/img/music-placeholder.png"
-        }
+        src={props.path ? props.path : Placeholder}
         alt="Currently playing song's album cover."
       />
     </div>
