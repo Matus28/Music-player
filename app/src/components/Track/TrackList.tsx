7b473577389421fe31ba49e67f0f0c013976e05a
Matus Tatarko <matus.tatarko28@gmail.com>
2023-10-08 15:51:13 +0200

    refactor: adds Traclist Header selected by clicking on playlist

--- app/src/components/Track/TrackList.tsx
+++ app/src/components/Track/TrackList.tsx
@@ -1,6 +1,8 @@
+import * as React from "react";
 import { Playlist, Song } from "../../utils/types";
 import { TrackItem } from "./TrackItem";
 import styles from "./TrackList.module.scss";
+import { DeviceContext } from "../../context/DeviceContext";
 
 export const TrackList = (props: {
   data: Song[];
@@ -11,32 +13,41 @@ export const TrackList = (props: {
   playingPlaylistId: number | null;
   onSelect: (id: number, playlistId: number) => void;
 }): JSX.Element => {
+  const isMobile = React.useContext(DeviceContext);
+
   return (
-    <ul className={styles.list}>
-      {props.data &&
-        props.data.map((song: Song, i: number) => (
-          <TrackItem
-            key={song.songId}
-            index={i + 1}
-            playlist={props.playlist}
-            playlists={props.playlists}
-            title={song.songName}
-            playlistId={props.playlist.playlistId}
-            playlistTitle={props.playlist.playlistName}
-            songId={song.songId}
-            interpret={song.songArtist}
-            songLength={song.songLength}
-            isPlaying={props.isPlaying}
-            isActive={
-              props.selectedSong &&
-              props.selectedSong.songId === song.songId &&
-              props.playlist.playlistId === props.playingPlaylistId
-            }
-            onItemClick={() => {
-              props.onSelect(i, props.playlist.playlistId);
-            }}
-          />
-        ))}
-    </ul>
+    <>
+      {isMobile && (
+        <div className={styles.header}>
+          <h2>{props.playlist.playlistName}</h2>
+        </div>
+      )}
+      <ul className={styles.list}>
+        {props.data &&
+          props.data.map((song: Song, i: number) => (
+            <TrackItem
+              key={song.songId}
+              index={i + 1}
+              playlist={props.playlist}
+              playlists={props.playlists}
+              title={song.songName}
+              playlistId={props.playlist.playlistId}
+              playlistTitle={props.playlist.playlistName}
+              songId={song.songId}
+              interpret={song.songArtist}
+              songLength={song.songLength}
+              isPlaying={props.isPlaying}
+              isActive={
+                props.selectedSong &&
+                props.selectedSong.songId === song.songId &&
+                props.playlist.playlistId === props.playingPlaylistId
+              }
+              onItemClick={() => {
+                props.onSelect(i, props.playlist.playlistId);
+              }}
+            />
+          ))}
+      </ul>
+    </>
   );
 };
